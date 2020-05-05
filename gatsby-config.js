b99b8c978ce00ff208bd28954777a55732bd0563
archer-jsonderman <jsonderman@archeredu.com>
2020-05-04 20:20:39 -0500

    Add gatsby-plugin-google-marketing-platform

--- gatsby-config.js
+++ gatsby-config.js
@@ -76,7 +76,7 @@ module.exports = {
         }
       }
     },
-   {
+   /*{
 	    resolve: 'gatsby-plugin-google-tagmanager',
 	    options: {
 	      id: 'GTM-WVQZZ82',
@@ -96,32 +96,19 @@ module.exports = {
 	
 	      // Specify optional GTM environment details.
 	    },
-	},
-	/*{
-      resolve: `gatsby-plugin-google-gtag`,
-      options: {
-	       trackingIds:[
-	      "GTM-WVQZZ82"  
-        ],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-       
-        gtagConfig: {
-          anonymize_ip: true,
-          cookie_expires: 0,
-        },
-        // This object is used for configuration specific to this plugin
-        pluginConfig: {
-          // Puts tracking script in the head instead of the body
-          head: true,
-          // Setting this parameter is also optional
-          //respectDNT: true,
-          // Avoids sending pageview hits from custom paths
-          //exclude: ["/preview/**", "/do-not-track/me/too/"],
-        },
-      },
-    },*/
-	  
+	},*/
+	{
+		resolve: 'gatsby-plugin-google-marketing-platform',
+	    options: {
+	      dataLayer: {
+	        experimentId: process.env.GATSBY_EXPID||null,
+	        variationId: 0
+	      },
+	      tagmanager: {
+	        id: 'GTM-WVQZZ82'
+	      }
+	    }
+    },	  
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
