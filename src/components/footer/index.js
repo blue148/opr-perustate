8f59005aaa7be92ba04363a59d5d8b9b5f756435
archer-jsonderman <jsonderman@archeredu.com>
2020-06-29 14:28:08 -0500

    Update Footer with external css

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -4,32 +4,12 @@ import Logo from "../../images/peru-logo-online-final.svg"
 import './footer.scss'
 
 const StyledFooter = styled.footer`
-	background-color:${props=>props.theme.brandmediumblue};
-	color:${props=>props.theme.primaryblue};
-	order:25;
-`
+	`
 const LogoBox = styled.div`
-	width:150px;
+	
 `
 const LinkBox = styled.div`
-	grid-column:1;
-	grid-row:2/3;
 	
-	@media (min-width:768px){
-		grid-column:2/3;
-		grid-row:1;
-	}
-	p{
-		font-size:.7rem;
-		margin-bottom:.3rem;
-		text-align:center;
-		@media (min-width:768px){
-			text-align:left;
-		}
-		a{
-			color:white;
-			}
-	}
 
 `
 export default class Footer extends React.Component{
@@ -37,20 +17,20 @@ export default class Footer extends React.Component{
 
 	render(){
 		return(
-		  <StyledFooter className="footer">
-		  <div className="desktop-shim">
-		    <LogoBox>
-		    	<Logo/>
-		    </LogoBox>
-		    <LinkBox>
-			     <p>©{new Date().getFullYear()} Peru State College</p>
-				 <p><a href="https://www.peru.edu/about/equal-opportunity-policy" target="_blank" rel="noopener noreferrer">Equal Opportunity Policy</a></p>
-				 <p><a href="https://www.peru.edu/privacypolicy" target="_blank" rel="noopener noreferrer">Privacy Policy</a></p>
-				 <p><a href="https://www.peru.edu/titleix/" target="_blank" rel="noopener noreferrer">Title IX</a></p>
-				 <p>A Proud Member of the <a href="https://www.nscs.edu/" target="_blank" rel="noopener noreferrer">Nebraska State College System</a></p>
-			</LinkBox>
+		  <footer className="footer">
+			<div className="desktop-shim">
+			    <div className="logoBox">
+			    	<Logo/>
+			    </div>
+			    <div className="linkBox">
+				     <p>©{new Date().getFullYear()} Peru State College</p>
+					 <p><a href="https://www.peru.edu/about/equal-opportunity-policy" target="_blank" rel="noopener noreferrer">Equal Opportunity Policy</a></p>
+					 <p><a href="https://www.peru.edu/privacypolicy" target="_blank" rel="noopener noreferrer">Privacy Policy</a></p>
+					 <p><a href="https://www.peru.edu/titleix/" target="_blank" rel="noopener noreferrer">Title IX</a></p>
+					 <p>A Proud Member of the <a href="https://www.nscs.edu/" target="_blank" rel="noopener noreferrer">Nebraska State College System</a></p>
+				</div>
 			</div>
-		  </StyledFooter>
+		  </footer>
 		)
 	}
 } 
